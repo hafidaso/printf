c2aa70c79a998d708986bdee3cd3a743e14c3238
hafidaso <hafidabelaidagnaoui@gmail.com>
2023-04-25 10:00:33 -0700

    Modification

--- _printf.c
+++ _printf.c
@@ -1,49 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - print anything
+ * _printf - prints anything
  * @format: the format string
- * Return: number of byt printed
+ *
+ * Return: number of bytes printed
  */
-
 int _printf(const char *format, ...)
 {
-	int som = 0;
-	va_list args;
-	char *arg, *start;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
 	params_t params = PARAMS_INIT;
-	
+
 	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (arg = (char *)format; *arg; arg++)
+	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-		if (*arg != '%')
+		if (*p != '%')
 		{
-			som += _putchar(*arg);
+			sum += _putchar(*p);
 			continue;
 		}
-		start = arg;
-		arg++;
-		while (get_flag(arg, &params)) /* while char at arg is flag char */
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
 		{
-			arg++; /* next char */
+			p++; /* next char */
 		}
-		arg = get_width(arg, &params, ap);
-		arg = get_precision(arg, &params, ap);
-		if (get_modifier(arg, &params))
-			arg++;
-		if (!get_specifier(arg))
-			som += print_from_to(start, arg,
-				params.l_modifier || params.h_modifier ? arg - 1 : 0);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			som += get_print_func(arg, ap, &params);
+			sum += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-	return (som);
+	return (sum);
 }
+
